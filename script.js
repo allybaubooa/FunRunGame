31d7fe81a71dc9bb9cb81782d43617b60be2b491
allybaubooa <allyhusseinbaubooa@gmail.com>
2022-06-29 22:37:33 +0400

    added new animations and sound

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 
     let character = document.getElementById('dog');
     let block = document.getElementById('block');
-    let block2 = document.getElementById('block2');
-    let box = document.getElementById('layout');
     let initial_pos = block.getBoundingClientRect().left;
     let pos_dog = character.getBoundingClientRect().top;
-    let score = document.getElementById('score');
 
-
+// Movement of the block and score incrementation
     let pos_block = initial_pos;
-        score = 0;
+    let  score = 0;
     let move_left = setInterval(function (){
         if (pos_block <= -40){
             score++
             pos_block = initial_pos;
             // console.log(score);
-            document.querySelector('p').textContent = score;
+            document.querySelector('p').textContent = 'Score: '+ score.toString();
         }
         else {
             block.style.left = pos_block + 'px';
@@ -23,14 +20,14 @@
         pos_block--
     },0.1)
 
-
+// Character Jump
 function jump(){
         let pos = pos_dog;
-        let pos2 = 130;
+        let pos2 = pos_dog - 70;
     let move_top = setInterval(function (){
-        if (pos < 130) {
+        if (pos < pos2) {
             character.style.top = pos2 + 'px'
-            if (pos2 > 180){
+            if (pos2 > pos_dog){
                 clearInterval(move_top)
             }
             pos2++
@@ -42,6 +39,20 @@ function jump(){
     },0.1)
 }
 
+// Jump Sound
+function jump_sound(){
+        let audio = new Audio('https://www.themushroomkingdom.net/sounds/wav/smb/smb_jump-small.wav')
+        audio.play();
+}
+
+// Collide Sound
+function collide_sound(){
+        let audio = new Audio('https://themushroomkingdom.net/sounds/wav/smb/smb_bump.wav')
+        audio.play();
+}
+
+
+
 //     let block_width = block.getBoundingClientRect().left;
 //     let character_top = character.getBoundingClientRect().top;
 
@@ -51,13 +62,20 @@ let check_collision = setInterval(function (){
     let character_top = Math.round(character.getBoundingClientRect().top);
     // console.log(block_width);
 
-    if(block_width < 20 && block_width > 0  && character_top >= pos_dog){
-        alert("Lose");
+    if(block_width < 40 && block_width > 0  && character_top >= pos_dog){
+        collide_sound();
         clearInterval(check_collision);
         clearInterval(move_left);
+        alert("Game Over");
     }
 },0.1);
 
-//Increasing Score
+// Increasing Balls
 
-
+// let ball_size = setInterval(function (){
+//     console.log(score);
+//     if (score % 2 === 0 || score != 0){
+//         block.style.width = 30 + 'px';
+//         block.style.height = 30 + 'px';
+//     }
+// },0.5)
