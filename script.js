b165bfd4a93969b4995f4ca0a954a85d9fa30a7b
allybaubooa <allyhusseinbaubooa@gmail.com>
2022-07-01 16:47:34 +0400

    Some changes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
     let block = document.getElementById('block');
     let layout = document.getElementById('layout');
     let start_btn = document.getElementById('button-start');
+    let volume_off = document.getElementById('off');
 
     let initial_pos = block.getBoundingClientRect().left;
     let pos_dog = character.getBoundingClientRect().top;
@@ -10,22 +11,6 @@
     let layout_left = layout.getBoundingClientRect().left;
 
     // console.log(pos_dog);
-
-// Movement of the block and score incrementation
-    let pos_block = initial_pos - layout_left; // 780-200
-    let  score = 0;
-    let move_left = setInterval(function (){
-        if (pos_block <= -20){
-            score++
-            pos_block = initial_pos - layout_left;
-            // console.log(score);
-            document.querySelector('p').textContent = 'Score: '+ score.toString();
-        }
-        else {
-            block.style.left = pos_block + 'px';
-        }
-        pos_block--
-    },0.1)
 
 // Character Jump
 function jump(){
@@ -52,16 +37,37 @@ function jump_sound(){
         let audio = new Audio('https://www.themushroomkingdom.net/sounds/wav/smb/smb_jump-small.wav')
         audio.play();
 }
+//
+// volume_off.addEventListener("click", function (ev){
+//         let audio = new Audio('https://www.themushroomkingdom.net/sounds/wav/smb/smb_jump-small.wav')
+//         audio.pause();
+// })
 
-// Collide Sound
-// function collide_sound(){
-//         let audio = new Audio('https://themushroomkingdom.net/sounds/wav/smb/smb_bump.wav')
-//         audio.play();
-// }
 
 
-    //Check if both divs collide / Stop animation when they collide.
-let check_collision = setInterval(function (){
+//start game btn pressed. (added movement of block and collision cheque in the event listener)
+let start_layout = document.getElementById('layout_game');
+
+start_btn.addEventListener('click',function (ev){
+    start_layout.classList.toggle('hide');
+
+// Movement of the block and score incrementation
+        let pos_block = initial_pos - layout_left; // 780-200
+    let  score = 0;
+    let move_left = setInterval(function (){
+        if (pos_block <= -20){
+            score++
+            pos_block = initial_pos - layout_left;
+            // console.log(score);
+            document.querySelector('p').textContent = 'Score: '+ score.toString();
+        }
+        else {
+            block.style.left = pos_block + 'px';
+        }
+        pos_block--
+    },0.1)
+//Check if both divs collide / Stop animation when they collide.
+    let check_collision = setInterval(function (){
     let block_width = Math.round(block.getBoundingClientRect().left - layout_left);
     let character_top = Math.round(character.getBoundingClientRect().top - layout_top);
 
@@ -70,28 +76,20 @@ let check_collision = setInterval(function (){
         //collide_sound();
         clearInterval(check_collision);
         clearInterval(move_left);
-        //alert("Game Over");
+        alert("Game Over");
     }
 },0.1);
 
-// Increasing Balls
+    // Increasing Balls
 
-// let ball_size = setInterval(function (){
-//     let block_width = block.getBoundingClientRect().width
-//     let block_height = block.getBoundingClientRect().height
-//     console.log(block_width)
-//     console.log(score);
-//     if (score % 2 == 0 && score != 0){
-//         block.style.width = block_width + 'px';
-//         block.style.height = block_height + 'px';
-//     }
-//     block_width+=10
-//     block_height+=10
-// },0.5)
+let block_shape = setInterval(function (){
+    console.log(score);
+    if (score % 5 == 0 && score != 0){
 
-//start game btn pressed.
-let start_layout = document.getElementById('layout_game');
-
-start_btn.addEventListener('click',function (ev){
-    start_layout.classList.toggle('hide');
+        block.style.borderRadius = 0 + 'px';
+    }
+    else {
+        block.style.borderRadius = 30 + 'px';
+    }
+},0.5)
 })
